0e91e9a7de23c3540ac5c7dbdf2eed68abc83e32
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2025-11-25 19:15:18 +0700

    chore: add logisticRegression.py

---
+++ src/logisticRegression.py
@@ -0,0 +1,10 @@
+# Model: Logistic Regression
+# Description: Implementation for multiclass classification from scratch.
+# Algorithm: Softmax/Multinomial Logistic Regression.
+
+# Import Libraries
+import numpy as np
+import pandas as pd
+
+
+    
