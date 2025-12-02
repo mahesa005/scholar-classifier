6aaa959ce4f657544b1fea89974f779a6d0bd2e9
faawibowo <faawibowo@gmail.com>
2025-12-02 20:58:06 +0700

    feat: add save load method

--- src/core/base_model.py
+++ src/core/base_model.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import numpy as np
-from src.utils.metrics import f1_macro
+from utils.metrics import f1_macro
+import os   
+import pickle
 
 class BaseClassifier(ABC):
    
@@ -11,6 +13,25 @@ class BaseClassifier(ABC):
     @abstractmethod
     def predict(self, X:np.ndarray) -> np.ndarray:
         pass
+
+    def save_model(self, filename):
+        folder = os.path.dirname(filename)
+        if folder and not os.path.exists(folder):
+            try: os.makedirs(folder)
+            except OSError: pass
+        try:
+            with open(filename, 'wb') as f: pickle.dump(self, f)
+            print(f"Model BERHASIL disimpan ke: {filename}")
+        except Exception as e: print(f"Gagal menyimpan model: {e}")
+
+    @staticmethod
+    def load_model(filename):
+        if not os.path.exists(filename): return None
+        try:
+            with open(filename, 'rb') as f: return pickle.load(f)
+        except Exception as e: print(f"Gagal memuat model: {e}"); return None
+
+        
 
     def score(self, X: np.ndarray, y: np.ndarray) -> float:
         # Calculate F1 macro score
